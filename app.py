feebb1b0fc4aeeaa44919c1c44cfe1dfb3402ac5
Lohainy Silva <lohainy.office@gmail.com>
2026-02-10 01:25:05 +0000

    'Adiciona aplicativo Streamlit completo'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ hist_button = st.button('Criar histograma')
 if hist_button:
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
-    fig = px.histogram(car_data, x = 'Odometer')
+    fig = px.histogram(car_data, x = 'odometer')
 
     st.plot_chart(fig, use_container_width = True)
 
